47113f237cf2082c854d82b237b7c932265b1220
DennisPai <a0936103338@gmail.com>
2025-08-13 02:34:19 +0800

    fix: correct import path in ProtectedRoute after component reorganization
    - Fix import path from '../redux/slices/authSlice' to '../../redux/slices/authSlice'
    - Resolves Zeabur frontend build failure: Module not found error
    - Update import path to match new component structure after moving to common/ directory
    - Also includes yup package installation in package.json

--- frontend/src/components/common/ProtectedRoute.jsx
+++ frontend/src/components/common/ProtectedRoute.jsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Navigate, useLocation } from 'react-router-dom';
 import { CircularProgress, Box } from '@mui/material';
-import { getCurrentUser } from '../redux/slices/authSlice';
+import { getCurrentUser } from '../../redux/slices/authSlice';
 
 const ProtectedRoute = ({ children }) => {
   const dispatch = useDispatch();
